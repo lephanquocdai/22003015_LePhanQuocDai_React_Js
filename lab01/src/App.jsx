c0cb63c34068fe97943e252f8e7a3cf436b988a7
H82M19\Student <Student>
2025-02-19 08:39:08 +0700

    tuan2

---
+++ lab01/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [text, setText] = useState('')
+  const [name, setName] = useState('');
+    function handleAssign (e) {
+      setText(e. target.value);
+    }
+    function handleClick(){
+      setName(text);
+    }
+
+  return (
+    <>
+      < input onChange={handleAssign} type="text" />
+      <button onClick={handleClick} >Click</ button>
+      <span>{name}</span>
+     
+    </>
+  )
+}
+
+export default App
