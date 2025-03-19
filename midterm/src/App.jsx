ddad6f1a2742354adfddb7dbe6822a1deda327bd
H82M19\Student <Student>
2025-03-19 08:41:44 +0700

    midterm

---
+++ midterm/src/App.jsx
@@ -0,0 +1,60 @@
+import { useReducer, useState, useRef, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  var [data, setData]= useState([]);
+
+  useEffect (()=>{
+    var url = 'https://67da1f0735c87309f52b0841.mockapi.io/data';
+    fetch(url).then((res)=>{
+      return res.json();
+    }).then((dataApi)=>{
+      setData(dataApi);
+    })
+  },[])
+
+  function reducer(state, action){
+    console.log(action);
+    switch(action.type){
+      case '+': return {...state, count: state.count + 1};
+      case '-': return {...state, count: state.count - 1};
+      default: return state;
+    }
+  }
+  var [state, dispatch] = useReducer(reducer,{count:0});
+  var [name, setName] = useState(undefined);
+  var valueInput = useRef(0);
+  function handleAdd(){
+    dispatch({type:'+'});
+  }
+  function handleMinus(){
+    dispatch({type:'-'});
+  }
+  function handlClick(){
+    setName(valueInput.current.value);
+  }
+  return (
+    <>
+    <ul>
+      {
+        data.map((item, index)=>{
+          return <li key={index}>{item.name}</li>
+        })
+      }
+    </ul>
+      <input ref={valueInput} type="text" />
+      <button onClick={handlClick}>Click</button>
+      <br />
+      <span>{name}</span>
+      <br />
+      <span>{state.count}</span><br />
+      <button onClick={handleAdd} style={{backgroundColor:'green'}}>Increase</button>
+      <button onClick={handleMinus} style={{backgroundColor:'red'}}>Decrease</button>
+    </>
+  )
+}
+
+export default App
