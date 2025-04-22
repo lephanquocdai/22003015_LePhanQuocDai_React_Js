e13a13472a41434bbf7386a9c4bb5071c2a46081
lephanquocdai <qdai1209@gmail.com>
2025-04-22 21:15:00 +0700

    cau3Theme

--- tuan9new/src/App.jsx
+++ tuan9new/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Counter from './components/Counter';
 import TodoList from './components/TodoList';
+import Theme from './components/Theme';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
          <Routes>
            <Route path="/" element={<Counter />}/>
            <Route path="/todo" element={<TodoList />}/>
+           <Route path="/theme" element={<Theme />} />
          </Routes>
        </div>
        </div>
--- tuan9new/src/app/store.jsx
+++ tuan9new/src/app/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counterSlice';
 import todoReducer from '../features/todoSlice';
+import themeReducer from '../features/themeSlice';
  
 export default configureStore({
    reducer: {
      counter: counterReducer,
      todo:todoReducer,
+     theme: themeReducer,
    },
  });
